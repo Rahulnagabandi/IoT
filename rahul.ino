6cb88db9e593d747f4c7abd543f795a0814f4061
Rahulnagabandi <96414505+Rahulnagabandi@users.noreply.github.com>
2021-12-30 17:59:04 +0530

    Update rahul.ino

--- rahul.ino
+++ rahul.ino
@@ -18,29 +18,29 @@
 #include "BlynkEdgent.h"
 BLYNK_WRITE(V0){
 int pinValue1 = param.asInt();
-digitalWrite(D5,pinValue1);
+digitalWrite(D0,pinValue1);
 
 }
 BLYNK_WRITE(V1){
 int pinValue2 = param.asInt();
-digitalWrite(D6,pinValue2);  
+digitalWrite(D1,pinValue2);  
 }
 BLYNK_WRITE(V2){
 int pinValue3 = param.asInt();
-digitalWrite(D7,pinValue3);  
+digitalWrite(D2,pinValue3);  
 }
 BLYNK_WRITE(V3){
 int pinValue4 = param.asInt();
-digitalWrite(D8,pinValue4);  
+digitalWrite(D5,pinValue4);  
 }
 void setup()
 {
   Serial.begin(115200);
   delay(100);
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
   pinMode(D5,OUTPUT);
-  pinMode(D6,OUTPUT);
-  pinMode(D7,OUTPUT);
-  pinMode(D8,OUTPUT);
   BlynkEdgent.begin();
 }
 
